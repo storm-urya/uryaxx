f0d990dc844afa9bd854060bce6e2af1788880d9
storm-urya <pneto0104@gmail.com>
2021-02-21 19:19:11 -0300

    First commit

---
+++ src/help.js
@@ -0,0 +1,311 @@
+const //database
+	return `
+╔══✪〘 INFO 〙✪══
+║
+╠➥ X. 𝐁𝐎𝐓
+╠➥ *3.2*
+╠➥ 𝐃𝐎𝐍𝐎:  ⃬⃗Urya.X⚜️
+╠➥ _wa.me/+5527996584717_
+╠➥ 𝐒𝐓𝐀𝐓𝐔𝐒: ON
+║
+╠══✪〘 NOVIDADES 〙✪══
+║
+║1 *.msc*
+║2 *.Comandos de voz*
+║3 *.meme [corrigido]*
+║4 *.comandos sem prefix*
+║5 *.gbin [premium]*
+║5 *.pack [premium]*
+║6 *.destrava [premium]*
+║7 *.gpessoa [premium]*
+║8 *.wame*
+║9 *.spamcall*
+║10 *.play (nome da msc)*
+║
+╠══✪〘 MENU 〙✪══
+║
+║11 *.figu*
+║12 *.toimg*
+║13 *.darkjokes (memes aleatórios)*
+║14 *.memeindo*
+║15 *.tts*
+║16 *.lolih [on]*
+║17 *.nsfwloli [off]*
+║18 *.url2img*
+║19 *.leens [na legenda]*
+║20 *.wait [na legenda]*
+║21 *.setprefix*
+║
+╠══✪〘 OUTROS 〙✪══
+║
+║22 *.linkgp*
+║23 *.simih [1/0]*
+║24 *.marcar*
+║25 *.add [@]*
+║26 *.banir [@]*
+║27 *.promover [@]*
+║28 *.rebaixar*
+║29 *.admins*
+║30 *.marcar2*
+║31 *.bc [texto]* (ele faz uma ™)
+║32 *.marcar3*
+║33 *.bloqueados*
+║34 *.bloquear [@]*
+║35 *.desbloquear [@]*
+║36 *.limpar*
+║37 *.bc [ *texto* ]*
+║38 *.bemvindo [1/0]*
+║39 *.clonar [@]*
+║40 *.help1*
+║41 *.dono*
+║42 *.owner*
+║43 *.tts [texto]*
+║44 *.setnome*
+║45 *.termux*
+║46 *.setfoto*
+║47 *.grupoinfo*
+║48 *.ytmp4*
+║49 *.bomdia*
+║50 *.boanoite*
+║51 *.marcar*
+║52 *.marcar2*
+║53 *.marcar3*
+║
+╠══✪〘 IMAGENS 〙✪══
+║
+║54 *.loli* [off]
+║55 *.loli1*
+║56 *.hentai*
+║57 *.dono*
+║58 *.porno*
+║59 *.boanoite*
+║60 *.bomdia*
+║61 *.boatarde*
+║62 *.mia [aleatórias]*
+║63 *.rize [aleatórias]*
+║64 *.minato [aleatórias]*
+║65 *.boruto [aleatórias]*
+║66 *.hinata [aleatórias]*
+║67 *.sasuke [aleatórias]*
+║68 *.sakura [aleatórias]*
+║69 *.naruto [aleatórias]*
+║70 *.meme*   
+║71 *.lofi*
+║72 *.malkova*
+║73 *.canal*
+║74 *.nsfwloli1*
+║75 *.reislin*
+║
+╠══✪〘 INTELIGÊNCIA IA 〙✪══
+║
+║76 *.simih 1 (para ativar)*
+║77 *.simih 0 (para desativar)*
+║ *.simi (sua mensagem)*
+║
+╠══✪〘 EM TESTE 〙✪══
+║
+║78 *.*
+║79 *.*
+║80 *.*
+║
+╠══✪〘 PREMIUM 〙✪══
+║
+║81 *.dado*
+║82 *.cekvip*
+║83 *.premiumlist*
+║84 *.delete*
+║85 *.modapk*
+║86 *.indo10*
+║87 *.daftarvip [para virar Premium]*
+║88 *.qrcode*
+║89 *.chentai*
+║90 *.gcpf*
+║91 *.gbin*
+║92 *.pack*
+║93 *.destrava*
+║94 *.gpessoa*
+║
+╠══✪〘 GRUPO 〙✪══
+║
+║95 *.banir*
+║96 *.leveling [on/off]*
+║97 *.level*
+║98 *.add*
+║99 *.promover*
+║100 *.setfoto [na legenda]*
+║101 *.setname [texto]*
+║102 *.rebaixar*
+║103 *.admins*
+║104 *.marcar*
+║105 *.marcar2*
+║106 *.marcar3*
+║107 *.bemvindo [1/0]*
+║108 *.grupoinfo*
+║109 *.bomdia*
+║110 *.boatarde*
+║111 *.boanoite*
+║112 *.setdesc*
+║113 *.bug [sua mensagem]*
+║
+╠══✪〘 ESPECIFICO DO BOT 〙✪══
+║
+║114 *.bug [sua mensagem]*
+║115 *.clonar [@]*
+║116 *.dono*
+║117 *.ping [ver velocidade do bot]*
+║118 *.termux*
+║119 *.gay [@]*
+║120 *.wame*
+║121 *.map (nome)*
+║122 *.setppbot (marque uma img)*
+║123 *.pinterest (nome)*
+║124 *.desligar (so para o dono)*
+║125 *.timer*
+║
+╠══✪〘 MAIS ALGUNS 〙✪══
+║
+║126 *.neko*
+║127 *.ttp [texto]*
+║128 *.testime*
+║129 *.tomp3*
+║130 *.modoanime [on/off]*
+║131 *.modonsfw [on/off]*
+║132 *.happymod [jogo/app]*
+║133 *.rize*
+║134 *.ytsearch*
+║135 *.moddroid [jogo/app]*
+║136 *.xvideos [titulo]**
+║137 *.nomegp*
+║138 *.darkjokes (memes aleatórios)*
+║139 *.animecry*
+║140 *.gay1*
+║141 *.next*
+║142 *.alerta*
+║143 *.belle [img aleatórias]*
+║144 *.pronomeneu [texto]*
+║144 *.hobby*
+║
+╠══✪〘 COMANDOS DE VOZ 〙✪══
+║
+║145 *.ola*
+║146 *.bv*
+║147 *.tchau*
+║148 *.bem*
+║149 *.a*
+║150 *.fdp*
+║151 *.onich*
+║152 *.beat1*
+║153 *.glub*
+║
+╠══✪〘 OUTROS /2 〙✪══
+║
+║154 *.antilink [1/0]*
+║155 *.brainly [pergunta]*
+║156 *.antiracismo [on/off]*
+║157 *.setnomebot*
+║158 *.meme*
+║159 *.musicas*
+║
+╠══✪〘 YT MUSIC 〙✪══
+║
+╠══NOTA »
+║menu completo em .msc
+╠════════════════════
+║
+║160 *.hylander*
+║161 *.nikeb*
+║162 *.BanhoDeLeite*
+║163 *.AkDoFlamengo*
+║164 *.Nemo*
+║165 *.kalidade*
+║166 *.macaverde*
+║167 *.m4*
+║168 *.cmgremio*
+║169 *.cmflamengo*
+║
+║
+╠══✪〘 HACKER MENU 〙✪══
+║
+║170 *.fbcheker*
+║171 *.hackfb*
+║172 *.bruteforcefb*
+║173 *.toolsphishing*
+║174 *.terkeytermux*
+║175 *.socialfishv2*
+║176 *.tool*
+║
+╠══✪〘 STICKER MENU 〙✪══
+║
+║177 *.animecry* 
+║178 *.hubgif*
+║179 *.blowgif* 🔞
+║180 *.shota*
+║181 *.hentaifig* 🔞
+║182 *.nekofig* 🔞
+║183 *.lolifig*
+║
+╠══✪〘 INTERATIVOS 〙✪══
+║
+╠══NOTA »
+║Mandar a msg sem o prefixo
+╠════════════════════
+║
+║184 *bah*
+║185 *oii*
+║186 *bv*
+║187 *canta ai bot*
+║188 *grita*
+║189 *digita*
+║190 *machista*
+║
+╠══✪〘 OUTROS /3 〙✪══
+║
+║191 *.atris* 🔞
+║
+╠══✪〘 CMDS DE VÍDEO 〙✪══
+║
+╠══EDITS »
+║_*Say Cat*_
+╠════════
+║
+║192 *.say1*
+║193 *.say2*
+║194 *.say3*
+║195 *.say4*
+║
+╠══EDITS »
+║_*XXXTENTACIONS*_
+╠═══════════════
+║
+║196 *.xxx1*
+║197 *.xxx2*
+║
+╠══EDITS »
+║_*VARIADAS*_
+╠═══════════════
+║
+║198 *.teto1*
+║199 *.edit1*
+║200 *.edit2*
+║201 *.edit3*
+║202 *.edit4*
+╠═══════════════
+║
+║ *MAIS COMANDOS*
+║ *PRÓXIMA ATT*
+║
+╠══✪〘 𝗗𝗢𝗡𝗢 〙✪══
+║
+║ *NOME: Urya.X*
+║ *INSTA: Raviel_zx_*
+║ _WPP: wa.me/+5527996584717_
+║
+║  *"Peita ou respeita 🐊🚩*
+║  *"kibadores, vão se fuder"*
+║
+║
+╚═〘  X.URYA.X 〙`
+}
+
+//Online.database
+
